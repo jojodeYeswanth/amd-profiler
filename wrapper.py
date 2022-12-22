c72f10714544fe8d27b4e4b26c1e91092ebd0191
Gokulspattom <110624065+Gokulspattom@users.noreply.github.com>
2022-12-22 22:26:34 +0530

    Created wrapper.py with all function calls

---
+++ wrapper.py
@@ -0,0 +1,21 @@
+import os
+import wrapper_utils
+try:
+    path = os.path.join(os.getcwd())
+    output_path = os.path.join(path, "wrapper")
+
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+        print(output_path + " folder created.")
+
+    else:
+        print(output_path + " folder already exists.")
+
+    wrapper_utils.data_collector()
+    wrapper_utils.rename_workloadfile()
+    wrapper_utils.nativePlatformDetails()
+
+    wrapper_utils.tarCreation()
+
+except Exception as e:
+    print(e,'exception occured')
